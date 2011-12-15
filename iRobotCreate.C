425fbdb2cac8c2eb42d73adf573441f205c9203f
Sagar Pandya <sagargp@gmail.com>
2011-12-14 17:54:48 -0800

    Adding velocity commands

--- iRobotCreate.C
+++ iRobotCreate.C
@@ -1,14 +1,16 @@
 #include "iRobotCreate.H"
 #include <SerialPort.h>
+#include <nrt/Core/Util/MathUtils.H>
 
 using namespace nrt;
+using namespace irobotcreate;
 
 // ######################################################################
 iRobotCreateModule::iRobotCreateModule(std::string const& instanceName) :
   Module(instanceName),
-  itsOdometryBaseFrame(irobotcreate::BaseFrameParamDef, this),
-  itsOdometryOdomFrame(irobotcreate::OdomFrameParamDef, this),
-  itsSerialDev(irobotcreate::SerialDevParam, this, &iRobotCreateModule::serialDevCallback)
+  itsOdometryBaseFrame(BaseFrameParamDef, this),
+  itsOdometryOdomFrame(OdomFrameParamDef, this),
+  itsSerialDev(SerialDevParam, this, &iRobotCreateModule::serialDevCallback)
 { }
 
 // ######################################################################
@@ -22,6 +24,7 @@ void iRobotCreateModule::serialDevCallback(std::string const & dev)
 	try
 	{
 		itsSerialPort = std::make_shared<SerialPort>(dev);
+		itsSerialPort->Open(SerialPort::BAUD_57600);
 	}
 	catch(SerialPort::AlreadyOpen & e)
 	{
@@ -36,7 +39,44 @@ void iRobotCreateModule::serialDevCallback(std::string const & dev)
 		throw exception::BadParameter(std::string("Invalid Argument: ") + e.what());
 	}
 
-	itsSerialPort->Open(SerialPort::BAUD_115200);
+	if(!itsSerialPort->IsOpen()) throw exception::BadParameter("Failed to open serial port");
+
+	// Send the OI start command
+	itsSerialPort->WriteByte(128);
+
+	// Put the iRobot into full command mode
+	itsSerialPort->WriteByte(132);
+
+}
+
+// ######################################################################
+void iRobotCreateModule::onMessage(VelocityCommand msg)
+{
+	// 1/2 the wheelbase of the create
+	double const radius = .258/2.0; 
+
+	double const transvel = msg->linear.x();
+	double const rotvel   = msg->angular.z();
+
+	// The distance each wheel should travel to accomodate the rotational velocity
+	double const rotwheeldist = rotvel * radius; 
+
+	int const leftspeed  = std::round(nrt::clamped((transvel + rotwheeldist)/1000.0, -500.0, 500.0));
+	int const rightspeed = std::round(nrt::clamped((transvel - rotwheeldist)/1000.0, -500.0, 500.0));
+
+	std::string command = 
+	{
+		nrt::byte(145),
+		(rightspeed >> 8) && 0x00FF,
+		(rightspeed >> 0) && 0x00FF,
+		(leftspeed >> 8) && 0x00FF,
+		(leftspeed >> 0) && 0x00FF
+	};
+
+	{
+		std::lock_guard<std::mutex> lock(itsMtx);
+		itsSerialPort->Write(command);
+	}
 }
 
 // ######################################################################
@@ -51,6 +91,4 @@ void iRobotCreateModule::run()
 }
 
 NRT_REGISTER_MODULE(iRobotCreateModule);
-
-
 
