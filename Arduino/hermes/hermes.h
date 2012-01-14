9e663eff9d8b38975c60a9053de8ac603b1b5743
Chris <licy@usc.edu>
2012-01-13 19:51:24 -0800

    Can drive now

---
+++ Arduino/hermes/hermes.h
@@ -0,0 +1,13 @@
+#define BOUND_FORWARD   2000
+#define BOUND_STOP      1500
+#define BOUND_BACKWARD  1000
+
+#define BAUDRATE  115200
+
+// Pins
+#define LEFT_SERVO 9
+#define RIGHT_SERVO 10
+
+// Commands
+#define CMD_RESET     97
+#define CMD_SETSPEED  98 // {leftSpeed 0-127} {rightSpeed 0-127}
