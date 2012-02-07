2a31a528d6693451fc96196f624f9d2f4dea7f42
Sagar Pandya <sagargp@gmail.com>
2012-02-06 22:44:14 -0800

    fixes issue #3

--- Robotics/Utils/Joystick/Joystick.C
+++ Robotics/Utils/Joystick/Joystick.C
@@ -1,4 +1,7 @@
 #include "Joystick.H"
+#include <nrt/ImageProc/IO/ImageSink/DisplayImageSink.H>
+#include <nrt/ImageProc/Drawing/Text.H>
+#include <nrt/Core/Util/Time.H>
 
 using namespace nrt;
 using namespace joystick;
@@ -6,15 +9,23 @@ using namespace joystick;
 // ######################################################################
 JoystickModule::JoystickModule(std::string const & instanceName) : 
   Module(instanceName),
+  itsUpdateRate(UpdateRateParam, this),
+  itsDisplaySink(new DisplayImageSink),
+  axes({}),
+  buttons({}),
+  itsJoystickOpen(false),
   itsJoystickDev(JoystickDevParam, this, &JoystickModule::joystickDevCallback)
 {
-  // Empty
+  addSubComponent(itsDisplaySink);
 }
 
 // ######################################################################
 void JoystickModule::joystickDevCallback(std::string const & dev)
 {
-  std::lock_guard<std::mutex> lock(itsMtx);
+  std::lock_guard<std::mutex> _(itsMtx);
+  itsJoystickOpen = false;
+
+  char joy_name[80];
 
   if (dev == "")
     return;
@@ -26,37 +37,79 @@ void JoystickModule::joystickDevCallback(std::string const & dev)
   }
 
   // get the number of axes and buttons from the joystick driver
+  int num_axes;
   ioctl(joy_fd, JSIOCGAXES, &num_axes);
+  int num_buttons;
   ioctl(joy_fd, JSIOCGBUTTONS, &num_buttons);
+  ioctl(joy_fd, JSIOCGNAME(80), &joy_name);
+
+  axes.resize(num_axes);
+  buttons.resize(num_buttons);
+
+  NRT_INFO("Detected joystick: " << joy_name << " " << num_axes << " axes and " << num_buttons << " buttons.");
 
   // operate the joystick driver in non-blocking mode
   fcntl(joy_fd, F_SETFL, O_NONBLOCK);
+  itsJoystickOpen = true;
 }
-
+!s!@
 // ######################################################################
 void JoystickModule::run()
 {
+  NRT_INFO("Running : " << axes.size() << " [" << num_axes << "] , " << buttons.size() << " [" << num_buttons << "]");
+
   while(running())
   {
-    JoystickMessage::unique_ptr msg(new JoystickMessage);
-    msg->axes.reserve(num_axes);
-    msg->buttons.reserve(num_buttons);
-    {
-      std::lock_guard<std::mutex> lock(itsMtx);
+    std::lock_guard<std::mutex> _(itsMtx);
 
-      read(joy_fd, &js, sizeof(struct js_event));
+    auto waitTime = std::chrono::milliseconds(int(1000 / itsUpdateRate.getVal()));
+    auto endTime = nrt::now() + waitTime;
+
+    if(!itsJoystickOpen)
+    {
+      usleep(std::chrono::microseconds(waitTime).count());
+      continue;
+    }
+    
+    while(nrt::now() < endTime)
+    {
+      usleep(1000);
+      js_event js;
+      int n = read(joy_fd, &js, sizeof(struct js_event));
+      if(n <= 0)
+      {
+        if(n == -1 && errno != EAGAIN) NRT_WARNING("Error reading from joystick");
+        continue;
+      }
 
       switch (js.type & ~JS_EVENT_INIT)
       {
         case JS_EVENT_AXIS:
-          msg->axes[js.number] = js.value;
+          if(js.number >= axes.size()) { NRT_WARNING("Out of bounds axes: " << js.number << " / " << axes.size()); continue; }
+          axes[js.number] = js.value;
           break;
 
         case JS_EVENT_BUTTON:
-          msg->buttons[js.number] = js.value;
+          if(js.number >= buttons.size()) { NRT_WARNING("Out of bounds axes: " << js.number << " / " << buttons.size()); continue; }
+          buttons[js.number] = js.value;
+          break;
+
+        default:
+          NRT_INFO("Unexpected result from joystick device");
           break;
       }
     }
+    y
+
+    Image<PixRGB<byte>> image(640, 480, ImageInitPolicy::Zeros);
+    for (int i = 0; i < axes.size(); i++)
+      nrt::drawText(image, Point2D<int32>(10, 20*i), nrt::sformat("axis %d: %d", i, axes[i])); 
+    itsDisplaySink->out(GenericImage(image), "Velocity Commander");
+
+    JoystickMessage::unique_ptr msg(new JoystickMessage);
+    msg->axes    = axes;
+    msg->buttons = buttons;
+
     post<JoystickCommand>(msg);
   }
 }
