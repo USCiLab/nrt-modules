f089f950bbd2df3808af7bd9ee2b27664bb04779
Chris <licy@usc.edu>
2012-02-08 19:12:13 -0800

    Gyro and compass data

--- Arduino/hermes/hermes.h
+++ Arduino/hermes/hermes.h
@@ -12,6 +12,10 @@
 #define CMD_RESET     97
 #define CMD_SETSPEED  98 // {leftSpeed 0-127} {rightSpeed 0-127}
 
+// Sensors
+#define SEN_COMPASS   99
+#define SEN_GYRO      100
+
 // Globals
 Servo Left;
 Servo Right;
@@ -19,7 +23,7 @@ Servo Right;
 // 
 // Debug 
 // 
-#define DEBUG
+// #define DEBUG
 
 #ifdef DEBUG
 #define LOG_RAW(message) Serial.write(message);
