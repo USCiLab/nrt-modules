f91251f7e262d28b328575631c6c63fd0d88ca45
Rand <rand@ubuntu.(none)>
2012-02-10 10:15:06 -0800

    Adding a simple DeadReckoningModule skeleton

---
+++ Robotics/Navigation/DeadReckoning/DeadReckoningModule.C
@@ -0,0 +1,30 @@
+#include "DeadReckoningModule.H"
+#include <SerialPort.h>
+#include <nrt/Core/Util/MathUtils.H>
+
+using namespace nrt;
+using namespace deadreckoning; 
+
+// ######################################################################
+DeadReckoningModule::DeadReckoningModule(std::string const& instanceName) :
+  Module(instanceName),
+  itsBaseFrameParam(BaseFrameParamDef, this),
+  itsDeadReckoningFrameParam(OutputFrameParamDef, this)
+{ }
+
+
+// ######################################################################
+void DeadReckoningModule::onMessage(VelocityCommand msg)
+{
+  std::lock_guard<std::mutex> _(itsMtx);
+
+  if(itsLastUpdateTime != nrt::Time())
+  {
+  }
+
+  itsLastVelocity = *msg;
+  itsLastUpdateTime = nrt::now();
+}
+
+NRT_REGISTER_MODULE(DeadReckoningModule);
+
