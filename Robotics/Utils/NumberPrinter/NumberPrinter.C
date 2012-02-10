9ce6b908c829b903c2768ca0bd5e52160aac3453
Sagar Pandya <sagargp@gmail.com>
2012-02-09 22:55:45 -0800

    added a module that just prints out nrt::real numbers that it gets

---
+++ Robotics/Utils/NumberPrinter/NumberPrinter.C
@@ -0,0 +1,21 @@
+#include "NumberPrinter.H"
+
+using namespace nrt;
+using namespace numberprinter;
+
+// ######################################################################
+NumberPrinterModule::NumberPrinterModule(std::string const & instanceName) :
+  Module(instanceName),
+  itsLabelParam(LabelParam, this)
+{ 
+  //
+}
+
+// ######################################################################
+void NumberPrinterModule::onMessage(numberprinter::InputMessage msg)
+{
+  std::lock_guard<std::mutex> _(itsMtx);
+  NRT_INFO(itsLabelParam.getVal() << ": " << msg->value);
+}
+
+NRT_REGISTER_MODULE(NumberPrinterModule);
