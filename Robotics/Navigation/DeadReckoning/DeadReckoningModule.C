f69ec8324e594bc324cd13fb2c11ec905440b326
Randolph Voorhies <voorhies@usc.edu>
2012-02-10 17:13:26 -0800

    Throw an exception if the user tries to set some velocities that are not translation in X or rotation about Z.

--- Robotics/Navigation/DeadReckoning/DeadReckoningModule.C
+++ Robotics/Navigation/DeadReckoning/DeadReckoningModule.C
@@ -18,11 +18,17 @@ void DeadReckoningModule::onMessage(VelocityCommand msg)
 {
   std::lock_guard<std::mutex> _(itsMtx);
 
+  // Uh oh! We can't handle full screw movement right now, so let's throw an exception if
+  // users are trying to mess around with other velocities
+  if(msg->linear.y() != 0 || msg->linear.z() != 0 || msg->angular.x() != 0 || msg->angular.y() != 0)
+    throw NRT_MODULE_EXCEPTION("Only linear.x and angular.z velocities are currently supported"); 
+
+
   if(itsLastUpdateTime != nrt::Time())
   {
   }
 
-  itsLastVelocity = *msg;
+  itsLastVelocity   = *msg;
   itsLastUpdateTime = nrt::now();
 }
 
