cb1eee16bcd105b51ce35b84ea4782d8fcac9dfe
Randolph Voorhies <voorhies@usc.edu>
2012-02-13 23:41:43 -0800

    Cleaned up the display method, and Transform grab
    Just need to implement the actual drawing method now, which is the fun part.

--- Robotics/Utils/TransformVisualizer/TransformVisualizerModule.C
+++ Robotics/Utils/TransformVisualizer/TransformVisualizerModule.C
@@ -3,8 +3,12 @@
 using namespace nrt;
 using namespace transformvisualizer;
 
-static gboolean transform_canvas_expose(GtkWidget * canvas, GdkEventExpose * event)
+// ######################################################################
+static gboolean 
+transform_canvas_expose(GtkWidget * canvas, GdkEventExpose * event, gpointer data)
 {
+  TransformVisualizerModule * visualizer = static_cast<TransformVisualizerModule*>(data);
+  Transform3D transform = visualizer->getTransform();
 
   // Get a cairo_t
   cairo_t * cr = gdk_cairo_create(canvas->window);
@@ -29,48 +33,9 @@ static gboolean transform_canvas_expose(GtkWidget * canvas, GdkEventExpose * eve
   cairo_set_source_rgb (cr, 0, 0, 255);
   cairo_stroke (cr);
 
-
   cairo_destroy(cr);
 
   return FALSE;
-}
-
-
-#define TRANSFORM_CANVAS_TYPE           (transform_canvas_get_type ())
-#define TRANSFORM_CANVAS(obj)           (G_TYPE_CHECK_INSTANCE_CAST ((obj), TRANSFORM_CANVAS_TYPE, TransformCanvas))
-#define TRANSFORM_CANVAS_CLASS(obj)     (G_TYPE_CHECK_CLASS_CAST ((obj), TRANSFORM_CANVAS,  TransformCanvasClass))
-#define IS_TRANSFORM_CANVAS(obj)        (G_TYPE_CHECK_INSTANCE_TYPE ((obj), TRANSFORM_CANVAS_TYPE))
-#define IS_TRANSFORM_CANVAS_CLASS(obj)  (G_TYPE_CHECK_CLASS_TYPE ((obj), TRANSFORM_CANVAS_TYPE))
-#define TRANSFORM_CANVAS_GET_CLASS      (G_TYPE_INSTANCE_GET_CLASS ((obj), TRANSFORM_CANVAS_TYPE, TransformCanvasClass))
-
-struct _TransformCanvas
-{
-  GtkDrawingArea parent;
-};
-
-struct _TransformCanvasClass
-{
-  GtkDrawingAreaClass parent_class;
-};
-
-typedef _TransformCanvas         TransformCanvas;
-typedef _TransformCanvasClass    TransformCanvasClass;
-G_DEFINE_TYPE (TransformCanvas, transform_canvas, GTK_TYPE_DRAWING_AREA);
-
-static void transform_canvas_class_init(TransformCanvasClass * canvas)
-{
-  GtkWidgetClass *widget_class;
-  widget_class = GTK_WIDGET_CLASS(canvas);
-  widget_class->expose_event = transform_canvas_expose;
-}
-
-static void transform_canvas_init(TransformCanvas *canvas)
-{
-}
-
-GtkWidget * transform_canvas_new(void)
-{
-  return static_cast<GtkWidget*>(g_object_new(TRANSFORM_CANVAS_TYPE, NULL));
 }
 
 // ######################################################################
@@ -78,9 +43,9 @@ TransformVisualizerModule::TransformVisualizerModule(std::string const & instanc
   Module(instanceName),
   itsFromParam(FromParamDef, this),
   itsToParam(ToParamDef, this)
-{ 
-}
+{ }
 
+// ######################################################################
 void TransformVisualizerModule::gtkThreadMethod()
 {
   g_thread_init(NULL);
@@ -89,13 +54,20 @@ void TransformVisualizerModule::gtkThreadMethod()
   int fakeargs = 0;
   gtk_init(&fakeargs, NULL);
   itsWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-  itsCanvas = transform_canvas_new();
+  itsCanvas = gtk_drawing_area_new();
   gtk_container_add(GTK_CONTAINER(itsWindow), itsCanvas);
-  g_signal_connect(itsWindow, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+  g_signal_connect(itsCanvas, "expose-event", G_CALLBACK(transform_canvas_expose), this);
   gtk_window_set_title(GTK_WINDOW(itsWindow), "Transform Visualizer");
   gtk_widget_show_all(itsWindow);
   gtk_main();
   gdk_threads_leave();
+}
+
+// ######################################################################
+Transform3D TransformVisualizerModule::getTransform()
+{
+  std::lock_guard<std::mutex> _(itsMtx);
+  return itsTransform;
 }
 
 // ######################################################################
@@ -115,10 +87,15 @@ void TransformVisualizerModule::run()
       if(results.size() == 0) { NRT_WARNING("No TransformManagers detected");        sleep(1); continue;}
       if(results.size() > 1)  { NRT_WARNING("Multiple TransformManagers detected!"); sleep(1); continue;}
       TransformMessage::const_ptr transform = results.get();
+      {
+        std::lock_guard<std::mutex> _(itsMtx);
+        itsTransform = transform->transform;
+      }
     }
     catch(...)
     {
       NRT_WARNING("Error looking up transform from [" << itsFromParam.getVal() << "] to [" << itsToParam.getVal() << "]");
+      sleep(1);
     }
   }
   
